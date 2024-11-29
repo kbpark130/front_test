134ae77161bc9b4b05df4cc50c92e7321869a346
kbpark130 <kbpark130@gmail.com>
2024-11-30 02:07:55 +0900

    fixed ps1 file

--- deploy_vue_app.ps1
+++ deploy_vue_app.ps1
@@ -4,13 +4,24 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 # Minikube 시작
-minikube start
+# minikube start --extra-config=apiserver.service-node-port-range=20000-40000
+# minikube start
+
+# vue-app 실패인 상태면
+kubectl delete service vue-app
+kubectl delete deployment vue-app
 
 # Vue 앱 배포 생성
 kubectl create deployment vue-app --image=kbpark130/vue-app
 
 # Vue 앱 서비스 노출
-kubectl expose deployment vue-app --type=NodePort --port=8081
+kubectl expose deployment vue-app --type=LoadBalancer --port=8081 --name=vue-app
 
 # Vue 앱 서비스 열기
-minikube service vue-app
+# minikube dashboard
+# minikube service vue-app
+
+# 끝낼 때
+# minikube stop
+# minikube delete
+
